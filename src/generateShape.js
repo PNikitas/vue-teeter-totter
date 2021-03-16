9eca9cb41a674938bc0a9072b010e69e4ec42a4e
Nikita Patlay <nikita.patlay@cheitgroup.com>
2021-03-16 15:18:24 +0300

    refactor(generateShape): styles refactoring

--- src/generateShape.js
+++ src/generateShape.js
@@ -1,4 +1,4 @@
-import { v4 } from 'uuid';
+import {v4} from 'uuid';
 
 import {MIN_WEIGHT, MAX_WEIGHT} from '@/helpers/constants';
 
@@ -15,21 +15,50 @@ export default () => {
     const weight = Math.floor(Math.random() * MAX_WEIGHT) + MIN_WEIGHT;
     const size = weight * 5 + 15;
 
-    const style = {
-        'width': type === 'triangle' ? '0' : `${size}px`,
-        'height': type === 'triangle' ? '0' : `${size}px`,
-        'font-size': type === 'triangle' ? (weight === 10 ? '28px' : `${size - 12}px`) : `${size - 5}px`,
-        'line-height': type === 'triangle' ? (weight === 10 ? '80px' : `${size + 6}px`) : `${size}px`,
+    const _defaultStyle = ({size, colors}) => ({
+        'width': `${size}px`,
+        'height': `${size}px`,
+        'font-size': `${size - 5}px`,
+        'line-height': `${size}px`,
         'font-weight': 'bold',
         'text-align': 'center',
-        'text-indent': type === 'triangle' ? `${-(size - 12)/4}px` : '0',
-        'border-width': type === 'triangle' ? `0 ${size / 2}px ${size}px ${size / 2}px` : '0',
+        'text-indent': '0',
+        'border-width': '0',
         'border-style': 'solid',
-        'border-color': type === 'triangle' ? `transparent transparent ${colors} transparent` : 'black',
-        'border-radius': type === 'circle' ? '50%' : '0%',
-        'background': type === 'triangle' ? 'none' : colors,
-        'box-shadow': type === 'triangle' ? 'inset 0 0 0 2px black' : 'none',
+        'border-color': 'black',
+        'border-radius': '0%',
+        'background': colors,
+        'box-shadow': 'none',
+    });
+
+    const _triangleStyle = ({weight, size, colors}) => ({
+        'width': '0',
+        'height': '0',
+        'font-size': (weight === 10 ? '28px' : `${size - 12}px`),
+        'line-height': (weight === 10 ? '80px' : `${size + 6}px`),
+        'text-indent': `${-(size - 12) / 4}px`,
+        'border-width': `0 ${size / 2}px ${size}px ${size / 2}px`,
+        'border-color': `transparent transparent ${colors} transparent`,
+        'background': 'none',
+        'box-shadow': 'inset 0 0 0 2px black',
+    });
+
+    const _circleStyle = ({}) => ({'border-radius': '50%'});
+
+    const _emptyStyle = ({}) => {};
+
+    const _specificStyle = {
+        'circle': _circleStyle,
+        'square': _emptyStyle,
+        'triangle': _triangleStyle,
     };
+
+    const getStyle = opts => ({
+        ..._defaultStyle(opts),
+        ..._specificStyle[opts.type](opts),
+    });
+
+    const style = getStyle({type, size, weight, colors});
 
     return {id, type, shapePositionX, weight, size, style};
 };
